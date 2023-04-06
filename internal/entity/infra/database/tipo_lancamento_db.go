916aaefb945185bfcd6f2baead29ca5ffd04fb87
Fabio Mattos <softvision.fpolis@gmail.com>
2023-04-06 15:45:34 -0300

    feat: insert and update database

--- internal/entity/infra/database/tipo_lancamento_db.go
+++ internal/entity/infra/database/tipo_lancamento_db.go
@@ -1,1 +1,39 @@
 package database
+
+import (
+	"database/sql"
+
+	"github.com/fabio-mattos/ContabilGoBack/internal/entity"
+)
+
+func InsertTipoLancamento(db *sql.DB, tipoLancamento *entity.TipoLancamento) error {
+	stmt, err := db.Prepare("insert into en_tipo_lancamento	(cd_tipo_lancamento,	cd_empresa ,	nm_tipo_lancamento ,	dt_cadastro , " +
+		"id_usario_cad ,	fg_ativo ,	id) 	values 	(	?,	?,	?,	?,	?,	?,	?	)")
+
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(tipoLancamento.CdTipoLancamento, tipoLancamento.CdEmpresa, tipoLancamento.NmTipoLancamento, tipoLancamento.DtCadastro, tipoLancamento.IdUsuarioCad, tipoLancamento.FgAtivo, tipoLancamento.Id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func FindTipoLancamentoByNome(db *sql.DB, nome string) (*entity.TipoLancamento, error) {
+	stmt, err := db.Prepare("select cd_tipo_lancamento,	cd_empresa,	nm_tipo_lancamento,	dt_cadastro, id_usario_cad,	fg_ativo, id " +
+		" from en_tipo_lancamento where lower(sem_acento(nm_tipo_lancamento)) = lower(sem_acento(?))")
+
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var t entity.TipoLancamento
+	err = stmt.QueryRow(nome).Scan(&t.CdTipoLancamento, &t.NmTipoLancamento, &t.CdEmpresa, &t.IdUsuarioCad, &t.IdUsuarioAlt, &t.DtCadastro, &t.DtAlteracao, &t.FgAtivo, &t.Id)
+
+	if err != nil {
+		return nil, err
+	}
+	return &t, nil
+}
--- internal/entity/infra/interfaces.go
+++ internal/entity/infra/interfaces.go
@@ -1,1 +1,8 @@
 package database
+
+import "github.com/fabio-mattos/ContabilGoBack/internal/entity"
+
+type TipoLancamentoInterface interface {
+	InsertTipoLancamento(tipoLancamento *entity.TipoLancamento) error
+	FindByNome(nome string) (*entity.TipoLancamento, error)
+}
